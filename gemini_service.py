2eb2160af6d6b0a0ddec9a064f632205c1eae80c
rajveersinghgkpssm-svg <rajveersinghgkpssm@gmail.com>
2026-06-14 14:04:03 +0530

    Refactor gemini_service.py to use environment variables

--- gemini_service.py
+++ gemini_service.py
@@ -1,35 +1,50 @@
+import os
+from dotenv import load_dotenv
 import google.generativeai as genai
 
-genai.configure(
-api_key="YOUR_API_KEY"
+load_dotenv()
+
+api_key = os.getenv(
+    "GEMINI_API_KEY"
 )
 
-model=genai.GenerativeModel(
-"gemini-1.5-pro"
+if not api_key:
+    raise ValueError(
+        "Gemini API key not found"
+    )
+
+genai.configure(
+    api_key=AQ.Ab8RN6Inob9P_P4SsZ9R5625HmtWdjzo11weQlIHDhzeH_0O6Q
+)
+
+model = genai.GenerativeModel(
+    "gemini-2.5-flash"
 )
 
 
 def ask_gemini(
-query,
-context
+    query,
+    context
 ):
 
-    prompt=f"""
+    prompt = f"""
 Context:
-
 {context}
 
 Question:
 {query}
-
-Provide:
-•⁠  ⁠Review
-•⁠  ⁠Errors
-•⁠  ⁠Improvements
 """
 
-    res=model.generate_content(
+    response = model.generate_content(
         prompt
     )
 
-    return res.text
+    return response.text
+
+
+
+
+print(
+os.getenv(
+"GEMINI_API_KEY"
+))
